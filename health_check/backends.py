18abf8dd8923012350c039dd172d958f6fecb0ad
Primož Kerin <primoz@datafy.it>
2017-01-17 11:02:38 +0100

    Move RetryHealthCheckBackend from base to backends

--- health_check/backends.py
+++ health_check/backends.py
@@ -6,7 +6,7 @@ import logging
 from django.utils.six import text_type
 from django.utils.translation import ugettext_lazy as _
 
-from health_check.exceptions import HealthCheckException
+from health_check.exceptions import HealthCheckException, ServiceReturnedUnexpectedResult, ServiceUnavailable
 
 logger = logging.getLogger('health-check')
 
@@ -54,3 +54,29 @@ class BaseHealthCheckBackend(object):
 
     def identifier(self):
         return self.__class__.__name__
+
+
+class RetryHealthCheckBackend(BaseHealthCheckBackend):
+    """
+    Extension of BaseHealthCheckBackend which supports retrying in case of any unexpected Exception
+
+    For this to work, you will need to implement `check_status_implementation` and return check result
+    """
+    retries = 3
+
+    def check_status_implementation(self):
+        raise NotImplementedError("Abstract method.")
+
+    def check_status(self):
+        attempts = 0
+        while attempts < self.retries:
+            try:
+                return self.check_status_implementation()
+            except (ServiceUnavailable, ServiceReturnedUnexpectedResult) as exc:
+                raise exc
+            except Exception as e:
+                # If check could not be completed, retry it
+                logger.warning(e, extra={'attempt': attempts}, exc_info=True)
+                attempts += 1
+
+        self.add_error(ServiceUnavailable("Could not check status."))
